f472fc60a96a7b4ef2e1d970f0e747229e485564
Pranav Vashi <neobuddy89@gmail.com>
2018-10-05 19:38:07 +0530

    msm8998-common: Add support for OnePlus Camera
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -92,10 +92,13 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-    vendor.camera.aux.packagelist=com.oneplus.camera,org.lineageos.snap,com.google.android.Pixel2ModUrnyx05 \
+    vendor.camera.aux.packagelist=com.oneplus.camera,com.android.camera,com.google.android.Pixel2ModUrnyx05 \
+    camera.hal1.packagelist=com.oneplus.camera \
     persist.camera.mobicat=2 \
     persist.camera.stats.debugexif=3080192 \
-    persist.ts.rtmakeup=false
+    persist.ts.rtmakeup=false \
+    camera.disable_zsl_mode=true \
+    persist.sys.camera.preview.size=1920x1080
 
 # CNE
 PRODUCT_PROPERTY_OVERRIDES += \
