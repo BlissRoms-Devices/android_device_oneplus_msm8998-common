01d5098e992a2793fc81d6bf46e5b066c75f4c21
celtare21 <celtare21@gmail.com>
2018-10-27 13:50:10 +0300

    msm8998: prop: bring in velocity values

--- vendor_prop.mk
+++ vendor_prop.mk
@@ -286,3 +286,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # Wi-Fi
 PRODUCT_PROPERTY_OVERRIDES += \
     wifi.interface=wlan0
+
+# Fling
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.min.fling_velocity=160 \
+    ro.max.fling_velocity=20000
