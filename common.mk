7f61911ffb3fadd3d1c7db513c27c3a6402c94a7
TheREV3N <rev3nt3ch@gmail.com>
2018-12-29 14:03:30 -0800

    Add missing call for USB Accessory

--- common.mk
+++ common.mk
@@ -428,7 +428,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service.basic
+    android.hardware.usb@1.0-service.basic \
+    com.android.future.usb.accessory
 
 # Vendor properties
 -include $(LOCAL_PATH)/vendor_prop.mk
