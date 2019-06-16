cb39ab13c3a4e4ba56668e2908474a0e9135ba7d
REV3NT3CH <rev3nt3ch@gmail.com>
2019-06-15 19:42:37 -0700

    DeviceParts: Add OnePlus color calibration mode

--- DeviceParts/src/org/omnirom/device/Startup.java
+++ DeviceParts/src/org/omnirom/device/Startup.java
@@ -155,5 +155,7 @@ public class Startup extends BroadcastReceiver {
         restore(DCIModeSwitch.getFile(), enabled);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_NIGHT_SWITCH, false);
         restore(NightModeSwitch.getFile(), enabled);
+        enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_ONEPLUS_SWITCH, false);
+        restore(OneplusModeSwitch.getFile(), enabled);
     }
 }
