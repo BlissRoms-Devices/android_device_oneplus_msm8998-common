7328526e0b0bcace0e435a74460173c630b7aa20
SirRGB <sirrgb@proton.me>
2023-04-23 18:30:06 +0200

    msm8998-common: drop touchgestures
    * depends on lineage hw import

--- DeviceSettings/src/com/bliss/device/DeviceSettings/Constants.java
+++ DeviceSettings/src/com/bliss/device/DeviceSettings/Constants.java
@@ -18,26 +18,4 @@
 package com.bliss.device.DeviceSettings;
 
 public class Constants {
-
-    // Broadcast action for settings update
-    static final String UPDATE_PREFS_ACTION = "com.bliss.device.DeviceSettings.UPDATE_SETTINGS";
-
-    // Screen off Gesture actions
-    static final int ACTION_FLASHLIGHT = 1;
-    static final int ACTION_CAMERA = 2;
-    static final int ACTION_BROWSER = 3;
-    static final int ACTION_DIALER = 4;
-    static final int ACTION_EMAIL = 5;
-    static final int ACTION_MESSAGES = 6;
-    static final int ACTION_PLAY_PAUSE_MUSIC = 7;
-    static final int ACTION_PREVIOUS_TRACK = 8;
-    static final int ACTION_NEXT_TRACK = 9;
-    static final int ACTION_VOLUME_DOWN = 10;
-    static final int ACTION_VOLUME_UP = 11;
-    static final int ACTION_AMBIENT_DISPLAY = 12;
-
-    // Broadcast extra: keycode mapping (int[]: key = gesture ID, value = keycode)
-    static final String UPDATE_EXTRA_KEYCODE_MAPPING = "keycode_mappings";
-    // Broadcast extra: assigned actions (int[]: key = gesture ID, value = action)
-    static final String UPDATE_EXTRA_ACTION_MAPPING = "action_mappings";
 }
--- DeviceSettings/src/com/bliss/device/DeviceSettings/Startup.java
+++ DeviceSettings/src/com/bliss/device/DeviceSettings/Startup.java
@@ -36,7 +36,6 @@ public class Startup extends BroadcastReceiver {
     @Override
     public void onReceive(final Context context, final Intent bootintent) {
         boolean enabled = false;
-        TouchscreenGestureSettings.MainSettingsFragment.restoreTouchscreenGestureStates(context);
         SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(context);
         enabled = sharedPrefs.getBoolean(DeviceSettings.KEY_SRGB_SWITCH, false);
         restore(SRGBModeSwitch.getFile(), enabled);
@@ -84,15 +83,5 @@ public class Startup extends BroadcastReceiver {
             return;
         }
         Utils.writeValue(file, value);
-    }
-
-    private boolean hasRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        return preferences.getBoolean(ONE_TIME_TUNABLE_RESTORE, false);
-    }
-
-    private void setRestoredTunable(Context context) {
-        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(context);
-        preferences.edit().putBoolean(ONE_TIME_TUNABLE_RESTORE, true).apply();
     }
 }
